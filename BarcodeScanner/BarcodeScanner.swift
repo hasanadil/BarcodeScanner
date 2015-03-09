f57e4fd6191b526e555a1923c1f505e862116283
Hasan Adil <hasan@assemblelabs.com>
2015-03-09 16:28:11 -0400

    Initial import

---
+++ BarcodeScanner/BarcodeScanner.swift
@@ -0,0 +1,13 @@
+//
+//  BarcodeScanner.swift
+//  BarcodeScanner
+//
+//  Created by Hasan on 3/9/15.
+//  Copyright (c) 2015 Hasan Adil. All rights reserved.
+//
+
+import Cocoa
+
+class BarcodeScanner: NSObject {
+
+}
